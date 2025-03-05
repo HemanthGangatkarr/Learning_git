743e749d852fdc1ed1967e3623f280f863eadd2d
Hemanth <gangatkarhemanth@gmail.com>
2025-03-05 12:48:19 +0530

    \b2

--- test1.py
+++ test1.py
@@ -1,5 +1,1 @@
-print("hello hemu")
-print("this is my second change through branch2")
-#this is the newly added line of code from barnch1
-#im batman
-#this is my sencond commit through branch2 
+print("hello")
